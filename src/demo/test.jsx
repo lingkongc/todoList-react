637a4595ee8713330ec44726bf5f96a42fc31dd7
lingkongc <lk792764556@outlook.com>
2020-06-04 17:42:45 +0800

    2020-06-04

--- src/demo/test.jsx
+++ src/demo/test.jsx
@@ -14,17 +14,21 @@ class Dog extends React.Component {
         cursor: 'pointer',
       },
       flag: true,
+      date: new Date(),
     }
 
     this.eat = this.eat.bind(this)
+    this.tick = this.tick.bind(this)
+    this.handleLinkClick = this.handleLinkClick.bind(this)
   }
 
   eat() {
+    // 注意setState是浅合并，会完整替换state的子属性，同时完整保留兄弟属性
     this.setState({
       style: {
         marginTop: '1rem',
         padding: '1rem',
-        background: this.flag ? 'red' : 'blue',
+        background: this.flag ? 'blue' : 'red',
         color: 'white',
         cursor: 'pointer',
       },
@@ -32,16 +36,36 @@ class Dog extends React.Component {
     this.flag = !this.flag
   }
 
-  swim() {
-    console.log('I can swim')
+  handleLinkClick(e) {
+    e.preventDefault()
   }
 
+  tick() {
+    this.setState({
+      date: new Date(),
+    })
+  }
+
+  // 生命周期方法
+  // 组件挂载
+  componentWillMount() {}
+  // 组件已挂载
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000)
+  }
+  // 组件卸载
+  componentWillUnmount() {
+    clearInterval(this.timerID)
+  }
   render() {
     var state = this.state
-
     return (
       <div style={state.style} onClick={this.eat}>
+        <a href="https://wwww.baidu.com" onClick={this.handleLinkClick}>
+          我是一个链接
+        </a>
         <p>{this.props.name}</p>
+        <p>It is {state.date.toLocaleTimeString()}.</p>
       </div>
     )
   }
